976c126114cef35eacb9073534842ae67a97ea90
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-20 20:54:49 -0300

    Update config.py

--- notemusic/plugins/config.py
+++ notemusic/plugins/config.py
@@ -1,8 +1,8 @@
 import os
 from pyrogram import Client
 
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = str(os.environ.get("API_HASH"))
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 
-NoteBot = Client(BOT_TOKEN, API_ID, API_HASH)
+NoteMusic = Client(BOT_TOKEN, API_ID, API_HASH)
