11e91b191b5fa55dd9b36bf2dc0b2f89cb0384a7
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-20 20:41:08 -0300

    Delete config.py

--- notemusic/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pyrogram import Client
-
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-NoteBot = Client(BOT_TOKEN, API_ID, API_HASH)
