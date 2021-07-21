58550f57b5c7e4f535b303eb4eea97a55a64c3b6
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-20 22:44:26 -0300

    update client.py

--- notemusic/client.py
+++ notemusic/client.py
@@ -1,8 +1,28 @@
 import os
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = str(os.environ.get("API_HASH"))
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+class Config:
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = str(os.environ.get("API_HASH"))
+    BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 
-NoteMusic = Client(BOT_TOKEN, API_ID, API_HASH)
+class NoteMusic(Client):
+    def __init__(self):
+        kwargs = {
+            'api_id': Config.API_ID,
+            'api_hash': Config.API_HASH,
+            'session_name': Config.BOT_TOKEN,
+            'bot_token': Config.BOT_TOKEN
+        }
+        super().__init__(**kwargs)
+
+    async def start(self):
+        await super().start()
+
+    async def stop(self):
+        await super().stop()
+
+    async def sleep(self, msg):
+        await msg.reply("`Sleeping for (10) Seconds.`")
+        Config.HU_APP.restart()
+    
