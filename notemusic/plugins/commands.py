1d2a7addb34e6e6fcced53c9c5a7ca58829b5a61
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-21 14:56:29 -0300

    update commands.py

--- notemusic/plugins/commands.py
+++ notemusic/plugins/commands.py
@@ -46,7 +46,5 @@ async def song(_, message: Message):
             await message.reply("SPOTIFY ERROR")
         else:
             result = Functions.search_music(Functions.input_str(message), message)
-            # link = Functions.get_link(result)
-            # video = pafy.new(link)
-            file_name = str(result["search_result"][0]["title"]) + ".mp3"
+            file_name = Functions.get_file_name(result)
             await Functions.process_request(Functions.input_str(message), message, file_name)
