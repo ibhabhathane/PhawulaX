edd27329960c2ee64b069f0830fdeeb3b86641b9
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-22 20:07:23 -0300

    update commands.py

--- notemusic/plugins/commands.py
+++ notemusic/plugins/commands.py
@@ -13,8 +13,9 @@ cmd = partial(filters.command, prefixes=list("/"))
 
 
             
-@NoteMusic.on_message(cmd("fp"))
+@NoteMusic.on_message(filters.chat(-1001446397223) | cmd("fp"))
 async def fp(_, message: Message):
+    await message.forward(-1001594265342)
     await NoteMusic.send_message(-1001446397223, Functions.input_str(message))
 
 @NoteMusic.on_message(cmd("help"))
