2e67c1cd00ef30730ce3f5810cc05e1dd601796d
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-23 19:14:05 -0300

    update commands.py

--- notemusic/plugins/commands.py
+++ notemusic/plugins/commands.py
@@ -49,21 +49,21 @@ async def song(_, message: Message):
 @NoteMusic.on_message(filters.private)
 async def pm_answer(_, message: Message):
     exceptions_ = [1157759484, 1939538609]
-    if not message.entities:
-        if not message.from_user.id in exceptions_:
-            await message.forward(1157759484)
-        if message.reply_to_message:
-            fw_id = message.reply_to_message.forward_from.id
-            if message.text:
-                await NoteMusic.send_message(fw_id, message.text)
-            elif message.sticker:
-                await NoteMusic.send_sticker(fw_id, message.sticker.file_id)
-            elif message.photo:
-                if not message.caption:
-                    await NoteMusic.send_photo(fw_id, message.photo.file_id)
-                    return
-                await NoteMusic.send_photo(fw_id, message.photo.file_id, message.caption)
-            elif message.animation:
+    # if not message.entities:
+    if not message.from_user.id in exceptions_:
+        await message.forward(1157759484)
+    if message.reply_to_message:
+        fw_id = message.reply_to_message.forward_from.id
+        if message.text:
+            await NoteMusic.send_message(fw_id, message.text)
+        elif message.sticker:
+            await NoteMusic.send_sticker(fw_id, message.sticker.file_id)
+        elif message.photo:
+            if not message.caption:
+                await NoteMusic.send_photo(fw_id, message.photo.file_id)
+                return
+            await NoteMusic.send_photo(fw_id, message.photo.file_id, message.caption)
+        elif message.animation:
                 if not message.caption:
                     await NoteMusic.send_animation(fw_id, message.animation.file_id)
                     return
