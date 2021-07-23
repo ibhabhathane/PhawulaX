814f8f3b989dbd86cd112db8cff9f87a631c6acc
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-23 14:19:49 -0300

    update notemusic.py

--- notemusic/notemusic.py
+++ notemusic/notemusic.py
@@ -25,7 +25,7 @@ class Functions:
         search = SearchVideos(user_input, offset = 1, mode = "json", max_results = 1)
         if not search == None:
             return json.loads(search.result())
-        message.reply("Não encontrei a música.")
+        message.reply("Não encontrei a música.", quote=True)
 
     def get_link(result):
         return result['search_result'][0]['link']
