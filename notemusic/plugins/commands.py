b90abb582c75cdf4e8e96db489bbbe4756b9d583
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-07-24 02:12:30 -0300

    update commands.py

--- notemusic/plugins/commands.py
+++ notemusic/plugins/commands.py
@@ -14,7 +14,7 @@ async def report(_, message: Message):
     if Functions.input_str(message) != "":
         rtext: Message = await message.forward(-1001578295861)
         nome = f"{message.from_user.first_name} {message.from_user .last_name}" if message.from_user.last_name else message .from_user.first_name
-        await rtext.reply(f"Pessoa que reportou:\n  id: {message.from_user.id}\n  Nome: {nome}\n  👤: @{message.from_user.username}")
+        await rtext.reply(f"Pessoa que reportou:\n  id: `{message.from_user.id}`\n  Nome: {nome}\n  👤: @{message.from_user.username}")
         report_text = "✅**VOCÊ FEZ UM REPORT**\n__Obrigado por reportar!__\n\nEsta mensagem já foi encaminhada para o mantenedor do projeto.  Ele poderá entrar em contato com você, se ele quiser. Mas não coloco muita fé. Obrigado e siga sua vida."
         await message.reply(report_text, quote=True)
         return
