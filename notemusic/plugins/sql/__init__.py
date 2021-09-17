adaf2ee4759039119b25ade08a87be4a605fa3c4
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-17 15:41:29 -0300

    update __init__.py

--- notemusic/plugins/sql/__init__.py
+++ notemusic/plugins/sql/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-import db
+from sql import db
 
 # USO OBRIGATÓRIO 
 DATABASE_URL = db# os.environ.get("DATABASE_URL")
