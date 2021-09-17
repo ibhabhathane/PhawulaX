c377c87412600adcb052ffc34b7c8d810729120d
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-17 17:45:06 -0300

    update db.py

--- notemusic/plugins/sql/db.py
+++ notemusic/plugins/sql/db.py
@@ -3,8 +3,8 @@ from . import SESSION, BASE
 
 class database(BASE):
     __tablename__ = "database"
-    database_website = Column(String, primary_key=True)
-    database_link = Column(String)
+    website = Column(String, primary_key=True)
+    link = Column(String)
 
     def __init__(self, website, link):
         self.website = website
