d1967daf8a99454a4285dc96db3ccd1e179acab5
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-17 17:18:26 -0300

    update noticia.py

--- notemusic/plugins/noticia.py
+++ notemusic/plugins/noticia.py
@@ -21,7 +21,7 @@ if db.get_link(feed_url) == None:
 def verificar_postar():
     FEED = feedparser.parse(feed_url)
     entry = FEED.entries[0]
-    if entry.id != db.get_link(entry.id):
+    if entry.id != db.get_link(entry.link):
 # CONFIGURE ESTA PARTE COMO DESEJAR
 # Tag para Resumo:{entry.summary}
       message = f"""
