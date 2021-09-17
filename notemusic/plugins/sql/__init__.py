691d5026325c45c5c508f9a097ea0ca02acf1415
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-17 15:23:12 -0300

    Create __init__.py

---
+++ notemusic/plugins/sql/__init__.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import db
+
+# USO OBRIGATÓRIO 
+DATABASE_URL = db# os.environ.get("DATABASE_URL")
+
+def start() -> scoped_session:
+    engine = create_engine(DATABASE_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    print("DATABASE_URL não foi configurada.")
+    print(str(e))
