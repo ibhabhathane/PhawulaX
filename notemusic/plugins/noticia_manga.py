0324ccad2ee0579625d9e1620f7f8ce350cff7d7
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-09-18 23:38:10 -0300

    update noticia_manga.py

--- notemusic/plugins/noticia_manga.py
+++ notemusic/plugins/noticia_manga.py
@@ -9,7 +9,8 @@ from notemusic import NoteMusic
 
 
 # CONFIGURAÇÃO IMPORTANTE 
-feed_url = "https://mangatube.site/feed"
+# feed_url = "https://mangatube.site/feed"
+feed_urls = list(set(i for i in os.environ.get("FEED_URLS").split("|")))
 log_channel = "-1001446397223"# "-1001165341477"  # Canal do Bot+ BotAdmin
 check_interval = 190
 max_instances = 200 
@@ -18,13 +19,14 @@ if db.get_link(feed_url) == None:
   db.update_link(feed_url, "*")
 
 # AQUI É ONDE É EXIBIDO O POST APÓS CHECK DE URL/FEED
-def verificar_postar():
-    FEED = feedparser.parse(feed_url)
-    entry = FEED.entries[0]
-    if entry.id != db.get_link(feed_url).link:
+def create_feed_checker(feed_url):
+    def verificar_postar():
+        FEED = feedparser.parse(feed_url)
+        entry = FEED.entries[0]
+        if entry.id != db.get_link(feed_url).link:
 # CONFIGURE ESTA PARTE COMO DESEJAR
 # Tag para Resumo:{entry.summary}
-      message = f"""
+            message = f"""
 **Novo cap de mangá, seu fi duma égua.**
 
 🎮 {entry.title}
@@ -32,17 +34,19 @@ def verificar_postar():
 
 ◾️ | <code>Mantido por:</code> @NoteZV
 """
-      try:
-        NoteMusic.send_message(log_channel, message)
-        db.update_link(feed_url, entry.id)
-      except FloodWait as e:
-        print(f"FloodWait: {e.x} segundos")
-        sleep(e.x)
-      except Exception as e:
-        print(e)
-    else:
-      print(f"FEED Verificado: {entry.id}")
+            try:
+                NoteMusic.send_message(log_channel, message)
+                db.update_link(feed_url, entry.id)
+            except FloodWait as e:
+                print(f"FloodWait: {e.x} segundos")
+                sleep(e.x)
+            except Exception as e:
+                print(e)
+        else:
+            print(f"FEED Verificado: {entry.id}")
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(verificar_postar, "interval", seconds=check_interval, max_instances=max_instances)
+for feed_url in feed_urls:
+    feed_checker = create_feed_checker(feed_url)
+    scheduler.add_job(feed_checker, "interval", seconds=check_interval, max_instances=max_instances)
 scheduler.start()
