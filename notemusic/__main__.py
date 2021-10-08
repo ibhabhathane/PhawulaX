e553db404d38a1b5cfd8133442c63168268c2343
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-07 22:38:59 -0300

    update __main__.py

--- notemusic/__main__.py
+++ notemusic/__main__.py
@@ -1,10 +1,11 @@
 from client import NoteMusic
 from notemusic import *
 
-# from plugins.commands import *
-# from plugins.creator_commands import *
-from plugins import *
+from plugins.commands import *
+from plugins.creator_commands import *
+from plugins.music_telethon import *
 # from plugins import noticia_manga
 
 if __name__ == "__main__":
 	NoteMusic.run()
+	c.run_until_disconnected()
