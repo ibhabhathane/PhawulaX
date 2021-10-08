1048f7538e5a7d1844e00b3502d70077280cd5aa
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-07 22:33:26 -0300

    update __main__.py

--- notemusic/__main__.py
+++ notemusic/__main__.py
@@ -1,8 +1,9 @@
 from client import NoteMusic
 from notemusic import *
 
-from plugins.commands import *
-from plugins.creator_commands import *
+# from plugins.commands import *
+# from plugins.creator_commands import *
+from plugins import *
 # from plugins import noticia_manga
 
 if __name__ == "__main__":
