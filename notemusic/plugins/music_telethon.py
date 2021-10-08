27886c9b7a5af732767b27e7455b2fae3d91d1ba
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-08 14:17:06 -0300

    update music_telethon.py

--- notemusic/plugins/music_telethon.py
+++ notemusic/plugins/music_telethon.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, events
 from youtubesearchpython import SearchVideos
+from notemusic import Functions
 import time
 import os
-import json
 import youtube_dl
 
 api_id = 3716600
@@ -17,49 +17,17 @@ def input_str(event):
         return str(input_.split(maxsplit=1)[1].strip())
     return ''
     
-def search_music(user_input):
-    search = SearchVideos(user_input, offset = 1, mode = "json", max_results = 1)
-    if not search.result() == None:
-        return json.loads(search.result())
-        
-def get_link(result):
-    if not result == None:
-        return result['search_result'][0]['link']
-        
-def get_file_name(result):
-        if not result == None:
-            title = result["search_result"][0]["title"]
-            if not ("[" and "]") in title:
-                return title + ".mp3"
-            else:
-                return title.replace(("]" or "["), "") + ".mp3"
-                
-def down_music(link, file_name):
-    _opts = {
-        "outtmpl": f"./cache/{file_name}",
-        "prefer_ffmpeg": True,
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            },
-        ],
-    }
-    with youtube_dl.YoutubeDL(_opts) as ydl:
-        info_dict = ydl.download([link])
 
 @c.on(events.NewMessage(pattern="^/song*"))
 async def song(event):
     if input_str(event) == "":
         await event.reply("Vou pesquisar o vento, arrombado?")
         return
-    result = search_music(event.text.split(maxsplit=1)[1])
-    link = get_link(result)
-    file_name = get_file_name(result)
+    result = Functions.search_music(event.text.split(maxsplit=1)[1])
+    link = Functions.get_link(result)
+    file_name = Functions.get_file_name(result)
     try:
-        down_music(link, file_name)
+        Functions.down_music(link, file_name)
     except:
         await event.reply("Num deu pra baixar...")
     if os.path.exists(f"./cache/{file_name}"):
