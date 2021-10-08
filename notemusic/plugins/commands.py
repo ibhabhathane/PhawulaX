60cc2f8fad0922224c8603ed1191fedbbb052ce7
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-08 15:00:41 -0300

    update commands.py

--- notemusic/plugins/commands.py
+++ notemusic/plugins/commands.py
@@ -5,22 +5,13 @@ from notemusic import Functions
 from client import NoteMusic
 
 from functools import partial, wraps
+
 import os
+
+import time 
 
 cmd = partial(filters.command, prefixes=list("/"))
     
-
-@NoteMusic.on_message(cmd("report"))
-async def report(_, message: Message):
-    if Functions.input_str(message) != "":
-        rtext: Message = await message.forward(-1001578295861)
-        report_text = "✅ **VOCÊ FEZ UM REPORT**\n__Obrigado por reportar!__\n\nEsta mensagem já foi encaminhada para o mantenedor do projeto.  Ele poderá entrar em contato com você, se ele quiser. Mas não coloco muita fé. Obrigado e siga sua vida."
-        await message.reply(report_text, quote=True)
-        
-        nome = f"{message.from_user.first_name} {message.from_user .last_name}" if message.from_user.last_name else message .from_user.first_name
-        await rtext.reply(f"▫️ **Pessoa que reportou:**\n  id: `{message.from_user.id}`\n  Nome: __{nome}__\n  👤: @{message.from_user.username}")
-        return
-    await message.reply("▫️ **COMANDO INVÁLIDO**\n\nReporte o erro após o comando.", quote=True)
 
 @NoteMusic.on_message(cmd("help"))
 async def help(_, message: Message):
@@ -31,10 +22,6 @@ async def help(_, message: Message):
 async def start(_, message: Message):
     start_text = "Oi!\nEu sou o [NoteMusic](t.me/notemusicbot)! Tudo bem?\n\n__Sou um Bot para baixar músicas, utilizo os serviços do YouTube para fornecer os resultados.__\n\n**Gostaria de saber mais sobre mim?**\nUtilize o comando /help"
     await message.reply(start_text, disable_web_page_preview=True)
-    
-    if message.chat.type == "private":
-        nome = f"{message.from_user.first_name} {message.from_user.last_name}" if message.from_user.last_name else message.from_user.first_name
-        await NoteMusic.send_message(-1001165341477, f"▫️ **Uma pessoa iniciou o seu bot.**\n\nid: `{message.from_user.id}`\nNome: __{nome}__\n👤: @{message.from_user.username}")
 
 @NoteMusic.on_message(cmd("music"))
 async def song(_, message: Message):
