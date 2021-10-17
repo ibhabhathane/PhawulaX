3701a23944f69a80581c88ba023bccd264274912
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-17 20:15:07 -0300

    update __main__.py

--- notemusic/__main__.py
+++ notemusic/__main__.py
@@ -2,7 +2,7 @@ from client import NoteMusic
 
 from plugins.commands import *
 from plugins.creator_commands import *
-from plugins.music_telethon import *
+# from plugins.music_telethon import *
 
 if __name__ == "__main__":
 	NoteMusic.run()
