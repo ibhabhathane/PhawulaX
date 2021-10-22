d3a7ffb04e6e25da273796e4c4a1bebe1e790ff3
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-22 00:51:23 -0300

    update notemusic.py

--- notemusic/notemusic.py
+++ notemusic/notemusic.py
@@ -85,7 +85,7 @@ class Functions:
         if os.path.exists(f"./notemusic/plugins/cache/{file_name}") and os.path.exists(f"./notemusic/plugins/cache/{thumb}"):
             try:
                 await NoteMusic.send_chat_action(message.chat.id, "upload_audio")
-                await message.reply_audio(audio=f"./notemusic/plugins/cache/{file_name}", caption=f"[Abrir no YouTube]({link})\n\n▫️ Atualizado pelo: @NoteZV", title=result[0]["title"], thumb=f"./notemusic/plugins/cache/{thumb}", quote=True)
+                await message.reply_audio(audio=f"./notemusic/plugins/cache/{file_name}", caption=f"[Abrir no YouTube]({link})\n\n▫️ Atualizado pelo: @NoteZV", title=result[0]["title"], thumb=f"./notemusic/plugins/cache/{thumb}", duration=duration, quote=True)
             except Exception as e:
                 await message.reply("❌ **ERRO**\n\nNão foi possível realizar o upload da música.", quote=True)
                 print(str(e))
