0a5acb3c7b317519634e721985e601944c42f0b0
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-30 22:22:39 -0300

    update client.py

--- notemusic/client.py
+++ notemusic/client.py
@@ -8,15 +8,15 @@ class Config:
     API_HASH = str("0ed423ceea4fbb06c0e627d9db0f4a6b")# str(os.environ.get("API_HASH"))
     BOT_TOKEN = str("2023772023:AAH0A_msjngj5XxqNEkpiobpcmVcMhRF5wU")# str(os.environ.get("BOT_TOKEN"))
 
-class NoteMusic(Client):
+class NoteBot(Client):
     def __init__(self):
-        name = self.__class__.__name__.lower()
         kwargs = {
-            'plugins': dict(root=f"{name}/plugins", exclude="music_telethon"),
+            'plugins': dict(root="plugins"),
             'api_id': Config.API_ID,
             'api_hash': Config.API_HASH,
             'session_name': ":memory:",
-            'bot_token': Config.BOT_TOKEN
+            'bot_token': Config.BOT_TOKEN,
+            'sleep_threshold': 60
         }
         super().__init__(**kwargs)
 
@@ -32,4 +32,4 @@ class NoteMusic(Client):
         await msg.reply("`Sleeping for (10) Seconds.`")
         Config.HU_APP.restart()
     
-NoteMusic = NoteMusic()
+NoteMusic = NoteBot()
