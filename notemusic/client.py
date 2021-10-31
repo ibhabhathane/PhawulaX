242aec9978561699c8f47d5855221e5d41f01abc
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-10-30 23:18:15 -0300

    update client.py

--- notemusic/client.py
+++ notemusic/client.py
@@ -11,7 +11,8 @@ class Config:
 class NoteBot(Client):
     def __init__(self):
         kwargs = {
-            'plugins': dict(root="notemusic.plugins")
+
+
             'api_id': Config.API_ID,
             'api_hash': Config.API_HASH,
             'session_name': ":memory:",
