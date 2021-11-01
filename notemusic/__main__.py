562856715fc92f09fa332a3e4d4b50733f9c1761
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-01 00:33:41 -0300

    update __main__.py

--- notemusic/__main__.py
+++ notemusic/__main__.py
@@ -2,5 +2,5 @@ from client import NoteMusic
 
 if __name__ == "__main__":
     from plugins.creator_commands import *
-	NoteMusic.run()
-	# c.run_until_disconnected()
+    NoteMusic.run()
+    # c.run_until_disconnected()
