02f1513ad09342722bd797405b22611a220e4c0a
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-04 21:23:00 -0300

    update notemusic.py

--- notemusic/notemusic.py
+++ notemusic/notemusic.py
@@ -78,7 +78,7 @@ class Functions:
     async def music_process(message):
         result = Functions.search_music(Functions.input_str(message))
         await message.reply(str(result))
-        if result == None:
+        if result == []:#is None:
             return await message.reply("Não foi possível encontrar a música.", quote=True)
         duration, dur = Functions.get_duration(result)
         if int(duration.split(":")[0]) >= 11 or len(duration) >= 7:
