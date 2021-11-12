c666f798889e267647ce4b52fa2992d669903f6b
Sommerfugl <57013747+ibhabhathane@users.noreply.github.com>
2021-11-12 15:22:11 -0300

    update notemusic.py

--- notemusic/notemusic.py
+++ notemusic/notemusic.py
@@ -90,7 +90,7 @@ class Functions:
         duration, dur = Functions.get_duration(result)
         if int(duration.split(":")[0]) >= 11 or len(duration) >= 7:
             return await message.reply("Músicas com duração acima de 10min não são permitidas. Use o YouTube ou pague meu host. Por este motivo, nem sonhe, não irei baixar essa desgraça.", quote=True)
-        link = Functions.get_link(result, message)
+        link = await Functions.get_link(result, message)
         filename = Functions.get_filename(result)
         thumb = Functions.get_thumb(result)
         try:
